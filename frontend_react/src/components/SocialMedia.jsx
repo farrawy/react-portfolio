5746a4b81729088dc51ac89746f4ede4da5b1843
Ahmed Elfarra <ahmed.e.alfarra@gmail.com>
2022-12-01 02:23:32 +0300

    social media links updated

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+
 import { FaFacebookF } from "react-icons/fa";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <BsTwitter />
+        <a href="https://www.linkedin.com/in/ahmadalfarra/" target="_blank">
+          <AiFillLinkedin />
+        </a>
       </div>
       <div>
+        <a href="https://github.com/farrawy" target="_blank">
+          <AiFillGithub />
+        </a>
+      </div>
+      <div>
+        <a target="_blank" href="https://twitter.com/istashajr">
+          <BsTwitter />
+        </a>
+      </div>
+      {/* <div>
+        <a href="https://facebook.com/"></a>
         <FaFacebookF />
-      </div>
+      </div> */}
       <div>
-        <BsInstagram />
+        <a href="https://instagram.com/istashajr" target="_blank">
+          <BsInstagram />
+        </a>
       </div>
     </div>
   );

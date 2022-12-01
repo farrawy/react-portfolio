77612e8cd6d3699026295bcac4f385f4e989badf
Ahmed Elfarra <ahmed.e.alfarra@gmail.com>
2022-12-01 22:34:27 +0300

    cv button uploaded

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -6,6 +6,8 @@ import "./Header.scss";
 
 import { AppWrap } from "../../wrapper";
 
+import PDFCV from "../../assets/ahmed elfarra.pdf";
+
 const scaleVariants = {
   whileInView: {
     scale: [0, 1],
@@ -17,56 +19,65 @@ const scaleVariants = {
   },
 };
 
-const Header = () => (
-  <div className="app__header app__flex">
-    <motion.div
-      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-      transition={{ duration: 0.5 }}
-      className="app__header-info"
-    >
-      <div className="app__header-badge">
-        <div className="badge-cmp app__flex">
-          <span>👋</span>
-          <div style={{ marginLeft: 20 }}>
-            <p className="p-text">Hello, I am</p>
-            <h1 className="head-text">Ahmed</h1>
+const Header = () => {
+  const downloadCV = () => {};
+  return (
+    <div className="app__header app__flex">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, I am</p>
+              <h1 className="head-text">Ahmed</h1>
+            </div>
           </div>
-        </div>
 
-        <div className="tag-cmp app__flex">
-          <p className="p-text">Web & Mobile Developer</p>
-          <p className="p-text">Freelancer</p>
-        </div>
-      </div>
-    </motion.div>
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Web & Mobile Developer</p>
+            <p className="p-text">Freelancer</p>
+          </div>
 
-    <motion.div
-      whileInView={{ opacity: [0, 1] }}
-      transition={{ duration: 0.5, delayChildren: 0.5 }}
-      className="app__header-img"
-    >
-      <img src={images.profile_memoji} alt="profile_bg" />
-      <motion.img
-        whileInView={{ scale: [0, 1] }}
-        transition={{ duration: 1, ease: "easeInOut" }}
-        src={images.circle}
-        alt="profile_circle"
-        className="overlay_circle"
-      />
-    </motion.div>
-
-    <motion.div
-      variants={scaleVariants}
-      whileInView={scaleVariants.whileInView}
-      className="app__header-circles"
-    >
-      {[images.react, images.sass, images.javascript].map((circle, index) => (
-        <div className="circle-cmp app__flex" key={`circle-${index}`}>
-          <img src={circle} alt={circle} />
+          <button className="tag-cmp app__flex" onClick={downloadCV}>
+            <a href={PDFCV} target="_blank">
+              <p className="p-text">Download CV</p>
+            </a>
+          </button>
         </div>
-      ))}
-    </motion.div>
-  </div>
-);
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.profile_memoji} alt="profile_bg" />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={images.circle}
+          alt="profile_circle"
+          className="overlay_circle"
+        />
+      </motion.div>
+
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {[images.react, images.sass, images.javascript].map((circle, index) => (
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt={circle} />
+          </div>
+        ))}
+      </motion.div>
+    </div>
+  );
+};
 
 export default AppWrap(Header, "home");
